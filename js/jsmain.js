d6c86d98e191a3c7ed3c609989a7fdf141cf3845
Sara Papp <sara.papp@jacobsmediagroup.com>
2018-11-16 17:16:24 +0000

    slider

--- js/jsmain.js
+++ js/jsmain.js
@@ -12,15 +12,24 @@ $(document).ready(function () {
         infinite: true,
         speed: 300,
         slidesToShow: 1,
+        slidesToScroll: 1,
         adaptiveHeight: true,
         arrows: true,
         swipe: true,
         responsive: [
             {
-                breakpoint: 500,
+                breakpoint: 9000,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
+                    fade: true,
+                    arrows: false,
+                    swipe: false
+                }
+            },
+            {
+                breakpoint: 1300,
+                settings: {
+                    arrows: true,
+                    swipe: true
                 }
             }
         ]
@@ -46,7 +55,7 @@ $(document).ready(function () {
                 }
             },
             {
-                breakpoint: 1023,
+                breakpoint: 1300,
                 settings: {
                     slidesToShow: 4,
                     slidesToScroll: 1
@@ -63,10 +72,10 @@ $(document).ready(function () {
         ]
     });
     detectMobile();
-    $(window).on('resize', function(){
+    $(window).on('resize', function () {
         detectMobile();
     });
-    $('.tab').hover (function() {
+    $('.tab').hover(function () {
         // add class to show for 1 microsecond the div with the full height
         $(this).find('.tab-text').addClass("js-height"); // 0 -> xxx
         var textheight = $(this).find('.tab-text').outerHeight(); // capture the xxx height
@@ -78,35 +87,72 @@ $(document).ready(function () {
 
         // animate from 0 to xxx
         $(this).find('.tab-text').stop().animate({
-            'height':textheight,
+            'height': textheight,
             'opacity': 1,
-        },250);
+        }, 250);
         $(this).find('.learn-more-btn').stop().animate({
-            'height':buttonheight,
+            'height': buttonheight,
             'opacity': 1,
-        },250);
-    }, function (){ // animate from 0 to xxx
+        }, 250);
+    }, function () { // animate from 0 to xxx
         $(this).find('.tab-text').stop().animate({
-            'height':0,
-            'opacity':0,
-        },250);
+            'height': 0,
+            'opacity': 0,
+        }, 250);
         $(this).find('.learn-more-btn').stop().animate({
-            'height':0,
-            'opacity':0,
-        },250);
+            'height': 0,
+            'opacity': 0,
+        }, 250);
     });
     // De-Focus arrows for the sliders because it affects the default arrow style
-    $(".slick-arrow").click(function(e){
+    $(".slick-arrow").click(function (e) {
         e.preventDefault();
-    }).focusin(function(e){
+    }).focusin(function (e) {
         $(this).blur();
     });
     //$(".tab .img-hover").each(function(){
-     //   var outer = ($(this).find('img').outerWidth() - $(this).outerWidth())/ 2;
-     //   $(this).find('img').css({'margin-left': outer * -1})
-   // });
+    //   var outer = ($(this).find('img').outerWidth() - $(this).outerWidth())/ 2;
+    //   $(this).find('img').css({'margin-left': outer * -1})
+    // });
+
+
+    $('.js-change-slide').click(function (e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        var index = $('.one-time').find(target).index();
+
+        $('.one-time').slick('slickGoTo', index);
+    });
+
+    // Transforms names list into a slider
+    $('.names').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: false,
+        arrows: false,
+        swipe: false,
+        vertical: true
+    });
+
+    // This binds the scroll wheel to the names list
+    $('.names').bind('mousewheel', function (e) {
+        // If is positive then it's scrolling up
+        if (e.originalEvent.wheelDelta > 0) {
+            $('.names').slick('slickPrev');
+        } else {
+            // Else is scrolling down
+            $('.names').slick('slickNext');
+        }
+        // Prevent scroll of the page, while scrolling on the element
+        e.stopPropagation();
+        e.preventDefault();
+    });
 });
-function detectMobile(){
+
+function detectMobile() {
     if ((/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()))) {
         $("main").addClass("mobile");
     } else {
